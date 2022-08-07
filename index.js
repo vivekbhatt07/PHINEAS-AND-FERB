d07961097e2d7130003b7b6b52350ddfe2078b3f
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-08 00:16:17 +0530

    add: input process output

---
+++ index.js
@@ -0,0 +1,23 @@
+const input_box = document.querySelector(".input-box");
+const output_box = document.querySelector(".output-box");
+const translate_btn = document.querySelector(".translate-btn");
+
+const textReceive = (text) => {
+  return (
+    "https://api.funtranslations.com/translate/ferb-latin.json" +
+    "?" +
+    "text=" +
+    text
+  );
+};
+
+translate_btn.addEventListener("click", function clickHandler() {
+  fetch(textReceive(input_box.value))
+    .then(function responseHandler(response) {
+      return response.json();
+    })
+    .then(function dataHandler(data) {
+      var translated = data.contents.translated;
+      output_box.innerHTML = translated;
+    });
+});
